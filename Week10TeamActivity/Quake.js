91da9e4c820283ea77921f718267925f483183ba
naishihchen <55642117+naishihchen@users.noreply.github.com>
2020-06-27 16:30:46 -0600

    Update Quake.js

--- Week10TeamActivity/Quake.js
+++ Week10TeamActivity/Quake.js
@@ -7,7 +7,7 @@ export default class Quake {
     // store the last batch of retrieved quakes in the model.  I don't always do this...in this case the api doesn't have an endpoint to request one quake.
     this._quakes = [];
   }
-  async getEarthQuakesByRadius(position, radius = 100) {
+  async getEarthQuakesByRadius(position, radius = 10000) {
     // use the getJSON function and the position provided to build out the correct URL to get the data we need.  Store it into this._quakes, then return it
     const query =
       this.baseUrl +
